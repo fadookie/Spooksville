fd7f9675ab7d6d89f1e3afebf1ee5c1c524f8a9d
Esteban Garcia <estebangarcia2121@gmail.com>
2019-10-18 23:43:23 -0700

    Created basic outline for Item and ItemManager class

---
+++ Developers/Unity Project/Spooksville/Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public Sprite itemSprite;
+
+    public float damage = 1.0f;
+    //Implement more weapon stats...
+
+    public virtual void Start()
+    {
+        WeaponManager.registerWeapon(this);
+    }
+}
---
+++ Developers/Unity Project/Spooksville/Assets/Scripts/Items/WeaponManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponManager : MonoBehaviour
+{
+    private static List<Weapon> weapons = new List<Weapon>();
+
+    public static void registerWeapon(Weapon weapon)
+    {
+        weapons.Add(weapon);
+    }
+}
