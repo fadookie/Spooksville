68b5842bbd8cb79159af2c441e284a2958f9c6d4
Stevemmmmm <estebangarcia2121@gmail.com>
2019-11-15 19:54:42 -0800

    Scene preperations | Art imports

--- Developers/Unity Project/Spooksville/Assets/Scripts/Items/Weapon.cs
+++ Developers/Unity Project/Spooksville/Assets/Scripts/Items/Weapon.cs
@@ -5,18 +5,7 @@ using UnityEngine;
 public class Weapon
 {
     public string Name { get; private set; }
-    public int Damage
-    {
-        get
-        {
-            return Damage;
-        }
-
-        private set
-        {
-
-        }
-    }
+    public int Damage { get; private set; }
     public int ID { get; private set; }
     
     public Weapon(string name, int damage)
