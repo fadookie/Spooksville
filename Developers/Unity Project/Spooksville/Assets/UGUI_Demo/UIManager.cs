d487eba40a6518cc1ff8e10b89f5fa8895f47bce
Eliot Lash <fadookie@gmail.com>
2019-11-17 16:29:12 -0800

    Add demo for UGUI multi-modal input handling

---
+++ Developers/Unity Project/Spooksville/Assets/UGUI_Demo/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UGUIDemo {
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject defaultSelection;
+    
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(defaultSelection);
+    }
+
+    public void OnButtonPress(Button target) {
+        Debug.LogFormat("OnButtonPress({0})", target.name);
+    }
+}
+}
