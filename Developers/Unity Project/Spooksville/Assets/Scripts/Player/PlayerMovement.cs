36e6493dc5f21c7b0cb904ece2ba8aecd949070d
Stevemmmmm <estebangarcia2121@gmail.com>
2019-12-02 23:44:30 -0800

    Scene completley done

--- Developers/Unity Project/Spooksville/Assets/Scripts/Player/PlayerMovement.cs
+++ Developers/Unity Project/Spooksville/Assets/Scripts/Player/PlayerMovement.cs
@@ -26,13 +26,13 @@ public class PlayerMovement : MonoBehaviour
 
         direction = new Vector3(x, y, 0f);
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(direction.x, 0f), Mathf.Abs(direction.x * speed * Time.deltaTime), LayerMask.GetMask(maskedLayers));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(direction.x, 0f), Mathf.Abs(direction.x * speed * Time.fixedDeltaTime), LayerMask.GetMask(maskedLayers));
         if (hit.collider == null)
         {
             transform.Translate(new Vector3(direction.x, 0f) * speed * Time.deltaTime);
         }
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(0f, direction.y), Mathf.Abs(direction.y * speed * Time.deltaTime), LayerMask.GetMask(maskedLayers));
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0f, new Vector2(0f, direction.y), Mathf.Abs(direction.y * speed * Time.fixedDeltaTime), LayerMask.GetMask(maskedLayers));
         if (hit.collider == null)
         {
             transform.Translate(new Vector3(0f, direction.y) * speed * Time.deltaTime);
